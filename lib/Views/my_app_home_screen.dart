26b4f88d422f475be7697bf3a2d2f466c4ff153f
shaimaHamila <hamilachaima18@gmail.com>
2024-12-01 21:02:11 +0100

    add explore card

--- lib/Views/my_app_home_screen.dart
+++ lib/Views/my_app_home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:recipe_app_flutter/Utils/constants.dart';
+import 'package:recipe_app_flutter/Widget/banner.dart';
+import 'package:recipe_app_flutter/Widget/my_icon_button.dart';
 
 class MyAppHomeScreen extends StatefulWidget {
   const MyAppHomeScreen({super.key});
@@ -10,6 +14,64 @@ class MyAppHomeScreen extends StatefulWidget {
 class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: [
+                  headerParts(),
+                  mySearchBar(),
+                  //For banner
+                  const BannerToExplore(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+
+  Padding mySearchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 22),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          prefixIcon: const Icon(Iconsax.search_normal),
+          fillColor: Colors.white,
+          border: InputBorder.none,
+          hintText: "Search any recipe",
+          hintStyle: TextStyle(color: Colors.grey.shade400),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none),
+        ),
+      ),
+    );
+  }
+
+  Row headerParts() {
+    return Row(
+      children: [
+        const Text(
+          "What are \ncooking todey?",
+          style:
+              TextStyle(fontSize: 32, fontWeight: FontWeight.bold, height: 1),
+        ),
+        const Spacer(),
+        //notification btn
+        MyIconButton(icon: Iconsax.notification, pressed: () {})
+      ],
+    );
   }
 }
---
+++ lib/Widget/banner.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app_flutter/Utils/constants.dart';
+
+class BannerToExplore extends StatelessWidget {
+  const BannerToExplore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 170,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15), color: kBannerColor),
+      //Using a Stack you can position widgets over one another.
+      child: Stack(
+        children: [
+          Positioned(
+            top: 24,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Cook The Best \nRecipes At Home",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 28),
+                    backgroundColor: Colors.white,
+                    elevation: 0,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Explore",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            right: -20,
+            child: Image.asset("assets/chef1.png"),
+          )
+        ],
+      ),
+    );
+  }
+}
