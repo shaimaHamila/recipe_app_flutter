bc6e7950466d6cf790e07765a81c4e693bd0b419
shaimaHamila <hamilachaima18@gmail.com>
2024-12-01 23:17:03 +0100

    add categories & fetch from firebase

--- lib/Views/my_app_home_screen.dart
+++ lib/Views/my_app_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:recipe_app_flutter/Utils/constants.dart';
@@ -12,30 +13,97 @@ class MyAppHomeScreen extends StatefulWidget {
 }
 
 class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
+  String category = 'All';
+  final CollectionReference categoriesItems =
+      FirebaseFirestore.instance.collection('Category');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Column(
-                children: [
-                  headerParts(),
-                  mySearchBar(),
-                  //For banner
-                  const BannerToExplore(),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    headerParts(),
+                    mySearchBar(),
+                    //For banner
+                    const BannerToExplore(),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "Categories",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    // For categories
+                    categoryList()
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      )),
+      ),
     );
+  }
+
+  StreamBuilder<QuerySnapshot<Object?>> categoryList() {
+    return StreamBuilder(
+        stream: categoriesItems.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          // If snapshot has data then show it otherwise the progress bar
+          if (streamSnapshot.hasData) {
+            return SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                      streamSnapshot.data!.docs.length,
+                      (index) => GestureDetector(
+                            onTap: () {
+                              //If the data is available it will works
+                              setState(() {
+                                category =
+                                    streamSnapshot.data!.docs[index]['name'];
+                              });
+                            },
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(18),
+                                  color: category ==
+                                          streamSnapshot.data!.docs[index]
+                                              ['name']
+                                      ? kprimaryColor
+                                      : Colors.white,
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 12),
+                                margin: const EdgeInsets.only(right: 18),
+                                child: Text(
+                                  streamSnapshot.data!.docs[index]['name'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 16,
+                                    color: category ==
+                                            streamSnapshot.data!.docs[index]
+                                                ['name']
+                                        ? Colors.white
+                                        : Colors.grey.shade800,
+                                  ),
+                                )),
+                          )),
+                ));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
   }
 
   Padding mySearchBar() {
@@ -43,6 +111,8 @@ class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
       padding: const EdgeInsets.symmetric(vertical: 22),
       child: TextField(
         decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
           filled: true,
           prefixIcon: const Icon(Iconsax.search_normal),
           fillColor: Colors.white,
@@ -63,10 +133,13 @@ class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
   Row headerParts() {
     return Row(
       children: [
-        const Text(
-          "What are \ncooking todey?",
-          style:
-              TextStyle(fontSize: 32, fontWeight: FontWeight.bold, height: 1),
+        const Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Text(
+            "What are \ncooking todey?",
+            style:
+                TextStyle(fontSize: 32, fontWeight: FontWeight.bold, height: 1),
+          ),
         ),
         const Spacer(),
         //notification btn
